ccd25504ec5c9af4174436b620eef682eb81487e
csanbar97 <cristianbs97@gmail.com>
2022-08-05 19:02:52 +0200

    class labels changed by className

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,11 +5,11 @@ export function Header(){
         <nav className="extra-margin">
           <div className="logo">Cristian Sánchez</div>
           <input type="checkbox" id="menu-toggle" />
-          <label for="menu-toggle" class="menu-button-container">
+          <label htmlFor="menu-toggle" className="menu-button-container">
             <div className="menu-button"></div>
           </label>
 
-          <ul class="menu">
+          <ul className="menu">
             <li><a href="/">Home</a></li>
             <li><a href="about">About</a></li>
             <li><a href="contact">Contact</a></li>
