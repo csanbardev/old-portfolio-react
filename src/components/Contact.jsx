07ffedccde67aba917ad02c244ee7c806dc6427d
csanbar97 <cristianbs97@gmail.com>
2022-08-09 19:43:06 +0200

    finished Contact component

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,31 @@
 import { Header } from "./Header";
+import { Footer } from "./Footer"
 
 export function Contact() {
   return (
     <>
       <Header />
+      <section id="contact-area" className="extra-margin">
+
+        <form id="contact-form" action="mailto:cristiansb97@outlook.com" method="post" encType="text/plain">
+          <h2>Contact me!</h2>
+          <input placeholder='NAME' type='text' required />
+          <input placeholder='EMAIL' type='email' required />
+          <textarea placeholder='WRITE YOUR MESSAGE' required />
+          <section className="contact-area--buttons">
+            <button className="btn primary-button" type="submit">Enviar</button>
+            <button className="btn primary-button" type="reset">Cancelar</button>
+          </section>
+          <section id="contact-info">
+            <h2>Contact info</h2>
+            <label>+34 798 789 789</label>
+            <label>cristiansb97@outlook.com</label>
+          </section>
+        </form>
+
+      </section>
+
+      <Footer />
     </>
   )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,7 +8,7 @@ export function Main() {
       <Works />
       <section className="extra-margin" id="contact">
         <h3 className="text">Want a talk? You're in the right place.</h3>
-        <button className="btn btn-primary">Get in touch</button>
+        <a href="contact" className="btn btn-primary">Get in touch</a>
       </section>
     </main>
   )
