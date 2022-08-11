e576adebbfc1a7ed6b6b7a54223f649a8f85b695
csanbar97 <cristianbs97@gmail.com>
2022-08-11 21:24:35 +0200

    fixed bug in url direction from project details to contact or about pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,14 +5,14 @@ export function Footer() {
       <nav id="footer--nav">
         <ul>
           <li><a href="/">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="contact">Contact</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
       </nav>
       <div id="footer--socialMedia">
-        <a href="https://github.com/csanbardev"><img src="/icons/github.png"/>GitHub</a>
-        <a href="https://www.linkedin.com/in/cristian-sánchez-barba/"><img src="/icons/linkedin.png"/>LinkedIn</a>
-        <a href="https://twitter.com/CristianSBDev"><img src="/icons/twitter.png"/>Twitter</a>
+        <a href="https://github.com/csanbardev"><img src="/icons/github.png" />GitHub</a>
+        <a href="https://www.linkedin.com/in/cristian-sánchez-barba/"><img src="/icons/linkedin.png" />LinkedIn</a>
+        <a href="https://twitter.com/CristianSBDev"><img src="/icons/twitter.png" />Twitter</a>
       </div>
     </footer>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,8 +11,8 @@ export function Header() {
 
         <ul className="menu">
           <li><a href="/">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="contact">Contact</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
         </ul>
         <div className="logo">Cristian Sánchez</div>
       </nav>
