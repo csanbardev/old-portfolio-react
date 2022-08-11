9493d9d89d0172134cdf041c6bdf5820a1f01604
csanbar97 <cristianbs97@gmail.com>
2022-08-11 21:19:32 +0200

    navigation changed with new design and so new styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,21 @@
 
-export function Header(){
-  return(
+export function Header() {
+  return (
     <header>
-        <nav className="extra-margin">
-          <div className="logo">Cristian Sánchez</div>
-          <input type="checkbox" id="menu-toggle" />
-          <label htmlFor="menu-toggle" className="menu-button-container">
-            <div className="menu-button"></div>
-          </label>
+      <nav className="extra-margin">
 
-          <ul className="menu">
-            <li><a href="/">Home</a></li>
-            <li><a href="about">About</a></li>
-            <li><a href="contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
+        <input type="checkbox" id="menu-toggle" />
+        <label htmlFor="menu-toggle" className="menu-button-container">
+          <div className="menu-button"></div>
+        </label>
+
+        <ul className="menu">
+          <li><a href="/">Home</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="contact">Contact</a></li>
+        </ul>
+        <div className="logo">Cristian Sánchez</div>
+      </nav>
+    </header>
   )
 }
