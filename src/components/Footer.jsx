cd2323f842db09981de0a45b72379a08f26ea018
csanbar97 <cristianbs97@gmail.com>
2022-08-12 17:45:30 +0200

    footer redesigned

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,19 +1,8 @@
 
 export function Footer() {
   return (
-    <footer className="extra-margin">
-      <nav id="footer--nav">
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-      <div id="footer--socialMedia">
-        <a href="https://github.com/csanbardev"><img src="/icons/github.png" />GitHub</a>
-        <a href="https://www.linkedin.com/in/cristian-sánchez-barba/"><img src="/icons/linkedin.png" />LinkedIn</a>
-        <a href="https://twitter.com/CristianSBDev"><img src="/icons/twitter.png" />Twitter</a>
-      </div>
+    <footer>
+      @{new Date().getFullYear()} Cristian Sánchez
     </footer>
   )
 }
