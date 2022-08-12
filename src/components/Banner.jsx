050912998a8a5123dc678d932f7d22a9c673426b
csanbar97 <cristianbs97@gmail.com>
2022-08-12 17:53:02 +0200

    few changes in banner component

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,7 +4,7 @@ export function Banner() {
     <section className="extra-margin" id="banner">
       <div className="banner-text-area">
         <p className="title-text">
-          Hi! I'm a frontend and backend web developer. Here below you can take a look to some of my recent projects and know more about me.
+          Hi! I'm <span className="tertiary-word">Cristian Sánchez</span>, web developer. Here below you can take a look to some of my <span className="tertiary-word">recent projects</span> and know more about me.
         </p>
       </div>
     </section>
