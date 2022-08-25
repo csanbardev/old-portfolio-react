cbacb62f6783e479b048c90919c7efdae6e4a3fe
csanbar97 <cristianbs97@gmail.com>
2022-08-25 19:14:22 +0200

    few changes in contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -33,10 +33,7 @@ export function ContactForm() {
         <h2>Contact me!</h2>
         <input placeholder='NAME' type='text' name="name" onChange={handleChange} value={form.name} onBlur={handleBlur} required />
         <input placeholder='EMAIL' type='email' name="email" onChange={handleChange} value={form.email} onBlur={handleBlur} required />
-        <textarea placeholder='WRITE YOUR MESSAGE' name="message" onChange={handleChange} value={form.message} onBlur={handleBlur} required />
-
-
-
+        <textarea cols={50} placeholder='WRITE YOUR MESSAGE' name="message" onChange={handleChange} value={form.message} onBlur={handleBlur} required />
         <section className="contact-area--buttons">
           <input className="btn primary-button" type="submit" value="Send" />
           <input className="btn primary-button" type="reset" value="Cancel" />
