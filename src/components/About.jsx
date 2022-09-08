fc6f45f6b271ebfbc039fdbb343ebd79b069a7ae
csanbar97 <cristianbs97@gmail.com>
2022-09-08 17:43:00 +0200

    changed font and some font-size properties

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,55 +7,79 @@ export function About() {
     <>
       <Header />
       <section id="about-banner" className="extra-margin">
-        <article>
+        <img src="/img/about.svg" />
+        <article id="about-banner-description">
           <h2>
             I'm Cristian Sánchez <br />
             Web Developer
           </h2>
           <p>
-            My Passion is to learn more and more about new tecnologies around web development.
-            I've studied spanish linguistic and literature also; that gave a new perspective about the world
-            and I tried to use it in my projects.
-            My main knowledge is frontend, although I've studied SQL, database design and a few NodeJS.
+            ¡Hola, soy Cristian! Mi gran pasión es crear páginas web únicas para todo tipo de usuarios.
+            Me enfoco principalmente en darle a mis trabajos un toque minimalista y atractivo que haga destacar
+            el contenido de la página.
           </p>
+          <p className="last">
+            Sobre mí, puedo decir que soy multidisciplinar y que no le temo a los retos. He estudiado anteriormente
+            filología hispánica, una carrera que ha cambiado mi forma de ver el mundo. Ahora me dedico de lleno al
+            desarrollo web, aunque en un futuro me gustaría unir ambos mundos.
+          </p>
+          <a className='btn btn-primary' href="/files/cv.pdf" download="Cristia Sánchez Barba CV">Get CV</a>
         </article>
-        <img src="" />
+
 
       </section>
       <h2 className="extra-margin">Skills</h2>
       <section className="extra-margin" id="about-skills">
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/html.svg' />
-          <h4>HTML</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/css.svg' />
-          <h4>CSS</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/javascript.svg' />
-          <h4>JavaScript</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/react.svg' />
-          <h4>React</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/vite.svg' />
-          <h4>Vite</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/mysql.svg' />
-          <h4>MySQL</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/java.svg' />
-          <h4>Java</h4>
-        </article>
-        <article>
-          <img className='about-skills--icon' src='icons/tecnologies/git.svg' />
-          <h4>Git</h4>
-        </article>
+        <section className="skills-front">
+          <h3>Frontend</h3>
+          <div className="skills-container">
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/html.svg' />
+              <h4>HTML</h4>
+            </article>
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/css.svg' />
+              <h4>CSS</h4>
+            </article>
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/javascript.svg' />
+              <h4>JavaScript</h4>
+            </article>
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/react.svg' />
+              <h4>React</h4>
+            </article>
+
+          </div>
+        </section>
+        <section className="skills-back">
+          <h3>Backend</h3>
+          <div className="skills-container">
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/mysql.svg' />
+              <h4>MySQL</h4>
+            </article>
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/java.svg' />
+              <h4>Java</h4>
+            </article>
+
+          </div>
+
+        </section>
+        <section className="skills-others">
+          <h3>Others</h3>
+          <div className="skills-container">
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/vite.svg' />
+              <h4>Vite</h4>
+            </article>
+            <article>
+              <img className='about-skills--icon' src='icons/tecnologies/git.svg' />
+              <h4>Git</h4>
+            </article>
+          </div>
+        </section>
       </section>
 
       <Footer />
